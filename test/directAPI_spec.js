da6dc60c596c68003b9cf7057b9ea0e39c41cccb
filnik <filippo.depretto@mavigex.com>
2013-11-08 19:20:57 +0100

    almost done the tests (the last one is not working for apparently no reason :( gonna fix it on monday).

--- test/directAPI_spec.js
+++ test/directAPI_spec.js
@@ -39,31 +39,29 @@ describe("base_api", function () {
     });
   });
 
-  it("should connect to mongoDB and return an empty collection", function (done) {
-    base.getFromModel({session: {}},
-      {
-        json: function (data) {
-          expect(data).to.eql([]);
-          done();
-        }
-      });
-  });
-
-  var createNewElement = function (callback, timestamp) {
-
+  var createNewElement = function (callback, timestamp, elementName) {
+    elementName = elementName || 'element1';
     var res = createRes(function (data){
-      expect(data['name']).to.eql('element1');
+      expect(data['name']).to.eql(elementName);
       callback(data);
     });
 
     base.newElementInModel(res,'element1',
       {
-        name: 'element1',
+        name: elementName,
         loc: {lat: 1, lng: 2},
         radius: 30,
         timestamp: timestamp
       });
   }
+
+  it("should connect to mongoDB and return an empty collection", function (done) {
+    base.getFromModel({session: {}}, createRes(
+      function (data) {
+          expect(data).to.eql([]);
+          done();
+      }));
+  });
 
   it("should add (only) a new element correctly", function (done) {
     var timestamp = new Date(),
@@ -99,4 +97,51 @@ describe("base_api", function () {
 
     }, timestamp);
   });
+
+  it("should get the last element inserted", function (done) {
+    var timestamp = new Date();
+
+    process.nextTick(function () {
+      var timestamp2 = new Date(),
+      res = createRes(function (data) {
+        done();
+      });
+      createNewElement(function(data){
+        createNewElement(function (data){
+          base.last(res);
+        }, timestamp2, 'element2');
+      }, timestamp, 'element1');
+    });
+  });
+
+  it("should remove an element correctly", function (done) {
+    var timestamp = new Date(),
+      res = createRes(function () {
+        var res = createRes(function(data) {
+          expect(data).to.eql([]);
+        });
+        base.getFromModel({session: {}}, res);
+        done();
+      });
+    createNewElement(function(data){
+      base.removeFromModel(res, data._id.toString());
+
+    }, timestamp);
+  });
+
+  it("should return an error if newConstraints is called without longitude or latitude", function (done) {
+    var constraints = base.nearConstraints(createRes(function (error) {
+      expect(error.error).to.eql('missing longitude or latitude');
+      done();
+    }));
+  });
+
+  it("should return correctly the contrains with nearConstrains", function (done) {
+    var constraints = base.nearConstraints({}, 1, 2);
+    expect(constraints.loc.$near[0]).to.eql(1);
+    expect(constraints.loc.$near[1]).to.eql(2);
+    expect(constraints.loc.$maxDistance).to.eql(30);
+    done();
+  });
+
 });
